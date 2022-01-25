98dc401708ed1af35c8729a593005104c7a59613
Starsdust <3425316708@qq.com>
2022-01-25 17:48:09 +0800

    项目初始化

---
+++ src/main/java/xyz/starsdust/void_awaken/VoidAwaken.java
@@ -0,0 +1,8 @@
+package xyz.starsdust.void_awaken;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(VoidAwaken.MOD_ID)
+public class VoidAwaken {
+    public static final String MOD_ID = "void_awaken";
+}
