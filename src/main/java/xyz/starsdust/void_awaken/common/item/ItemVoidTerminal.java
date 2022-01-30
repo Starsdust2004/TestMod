67cad07e879095866208ac90fa54bbd9d0a582c3
Starsdust <3425316708@qq.com>
2022-01-30 18:14:37 +0800

    添加测试物品

---
+++ src/main/java/xyz/starsdust/void_awaken/common/item/ItemVoidTerminal.java
@@ -0,0 +1,9 @@
+package xyz.starsdust.void_awaken.common.item;
+
+import net.minecraft.item.Item;
+
+public class ItemVoidTerminal extends Item {
+    public ItemVoidTerminal() {
+        super(new Properties().tab(ModItemGroup.MOD_ITEM));
+    }
+}
